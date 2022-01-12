dbc741f287b711ff2b019a0b51746a57d9fbbab5
matiasbrascescoglobant <matias.brascesco@globant.com>
2022-01-12 12:17:22 -0300

    Refactored project structure.

--- src/main/java/springboot/conduit/ConduitApplication.java
+++ src/main/java/springboot/conduit/ConduitApplication.java
@@ -1,4 +1,4 @@
-package mnbd.springboot.conduit;
+package springboot.conduit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/test/java/springboot/conduit/ConduitApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.conduit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConduitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
